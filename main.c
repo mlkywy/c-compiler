8ba8b2ca1f43f49305ddc42388ae5fd1b29ce258
alsheikhiu <alsheikh@iu.edu>
2023-04-25 23:37:46 -0400

    create test file

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "helpers/vector.h"
+#include "compiler.h"
 
 int main()
 {
-    struct vector *vec = vector_create(sizeof(int));
+    int res = compile_file("./test.c", "./test", 0);
 
-    int x = 50;
-    vector_push(vec, &x);
-    x = 20;
-    vector_push(vec, &x);
-
-    vector_set_peek_pointer(vec, 0);
-    int *ptr = vector_peek(vec);
-
-    while (ptr)
+    if (res == COMPILED_FILE_COMPILED_OK)
     {
-        printf("%i\n", *ptr);
-        ptr = vector_peek(vec);
+        printf("Everything compiled fine\n");
+    }
+    else if (res == COMPILER_FAILED_WITH_ERRORS)
+    {
+        printf("Compile failed\n");
+    }
+    else
+    {
+        printf("Unknown response for compile file\n");
     }
 
     return 0;
